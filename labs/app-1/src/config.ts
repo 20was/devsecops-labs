54801985ffd83879a557e7ae27db1e3c6140d5c2
Bishwas <20wasadk@gmail.com>
2026-07-26 12:27:43 +1000

    Add github token to config

--- labs/app-1/src/config.ts
+++ labs/app-1/src/config.ts
@@ -3,4 +3,5 @@ export const config = {
   awsAccessKeyId: 'AKIAIOSFODNN7EXAMPLE',
   awsSecretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
   region: 'ap-southeast-2',
+  githubToken: 'ghp_1234567890abcdefghijklmnopqrstuv12345',
 };

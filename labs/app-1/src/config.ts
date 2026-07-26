c3cf583dd992ad345e47b8a9008150b1ea461aba
Bishwas <20wasadk@gmail.com>
2026-07-26 12:17:22 +1000

    Add app config

---
+++ labs/app-1/src/config.ts
@@ -0,0 +1,6 @@
+// TODO: move to environment variables later
+export const config = {
+  awsAccessKeyId: 'AKIAIOSFODNN7EXAMPLE',
+  awsSecretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+  region: 'ap-southeast-2',
+};

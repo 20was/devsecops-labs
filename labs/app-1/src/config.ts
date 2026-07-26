cf1c1d3ffff3e3ec9f70795e86be64dd28e36b1b
Bishwas <20wasadk@gmail.com>
2026-07-26 12:42:20 +1000

    FIX: move secrets to env vars, add .env.example template

--- labs/app-1/src/config.ts
+++ labs/app-1/src/config.ts
@@ -1,7 +1,5 @@
-// TODO: move to environment variables later
 export const config = {
-  awsAccessKeyId: 'AKIAIOSFODNN7EXAMPLE',
-  awsSecretAccessKey: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
-  region: 'ap-southeast-2',
-  githubToken: 'ghp_1234567890abcdefghijklmnopqrstuv12345',
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION ?? 'ap-southeast-2',
 };
